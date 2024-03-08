ef8ef668e8e93d51f0e9cc6f1185ca68e849f9e2
JaiderArleyGonzalez <89174283+JaiderArleyGonzalez@users.noreply.github.com>
2024-03-08 14:45:49 +0000

    test

--- python/test_yatzy.py
+++ python/test_yatzy.py
@@ -4,7 +4,30 @@ from yatzy import Yatzy
 # available from http://pytest.org/
 
 
-def test_demo():
-        expected = 15
-        actual = 15
-        assert expected == actual        
+
+def test_Chance_SumOfFour():
+        expected = 14
+        actual = Yatzy.chance(1,1,3,3,6)
+        assert expected == actual
+
+def test_Yatzy_SameNumber():
+        expected = 50
+        dice = [1,1,1,1,1]
+        actual = Yatzy.yatzy(dice)
+        assert expected == actual
+
+def test_Yatzy_IsNotTheSameNumber():
+        expected = 0
+        dice = [1,1,1,2,1]
+        actual = Yatzy.yatzy(dice)
+        assert expected == actual
+
+def test_Ones_placedOnOnce():
+        expected = 0
+        actual = Yatzy.ones(3,3,3,4,5)
+        assert expected == actual
+
+def test_Two_PlacedOnTwos():
+        expected = 4
+        actual = Yatzy.twos(2,3,2,5,1)
+        assert expected == actual
